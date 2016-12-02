7bb05619ce542e5de2d3fda5401e43dc8e131856
Kevin Speight <kdspeight@hotmail.co.uk>
2016-12-02 14:43:20 +0000

    Added form validation to model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,7 @@
 class Contact < ActiveRecord::Base
-    
+  #can be left blank by default, but if validation is needed
+  #this is where it goes
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :comments, presence: true
 end
