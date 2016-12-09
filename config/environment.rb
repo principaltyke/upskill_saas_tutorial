02b2893addd8f0ca67754c1d7d412c6dbd6075f1
Kevin Speight <kdspeight@hotmail.co.uk>
2016-12-09 22:42:32 +0000

    Added mailer settings to environment file. Bug

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 #added as part of course to sned out emails
 ActionMailer::Base.smtp_settings = {
   :port           =>  ENV['MAILGUN_SMTP_PORT'],
-  :address        =>  ENV['MAILGUN_SMTP_SERVER'].
+  :address        =>  ENV['MAILGUN_SMTP_SERVER'],
   :user_name      =>  ENV['MAILGUN_SMTP_LOGIN'],
   :password       =>  ENV['MAILGUN_SMTP_PASSWORD'],
   :domain         =>  ENV['www.principaltyke.com'],
