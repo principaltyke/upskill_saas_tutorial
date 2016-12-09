bd49c5c9b56cfc6026adfb3accad9076759e5763
Kevin Speight <kdspeight@hotmail.co.uk>
2016-12-09 22:59:20 +0000

    Added mailer settings to environment file. Bug 5

--- config/environment.rb
+++ config/environment.rb
@@ -10,7 +10,6 @@ ActionMailer::Base.smtp_settings = {
   :address        =>  ENV['MAILGUN_SMTP_SERVER'],
   :user_name      =>  ENV['MAILGUN_SMTP_LOGIN'],
   :password       =>  ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         =>  ENV['www.principaltyke.com'],
   :authentication =>  :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
