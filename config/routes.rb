170f6cc2702dd9af13c8ca6e8543c8d3cafb9666
Kevin Speight <kdspeight@hotmail.co.uk>
2017-01-16 09:54:57 +0000

    Added routes for user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   #originally devise_for :users
   devise_for :users, controllers: { registrations: 'users/registrations' } # now we need to tell devise (extend) that when devise is about to register a user we need to know if pro or basic. we do this with a new controller called registrations
+  
+  #nested routes. These are routes to pull associated things
+  #together such as profiles would naturally fall under
+  #the user route
+  resources :users do
+    resource :profiles #singular
+  end
+  
   get 'about', to: 'pages#about'
   resources :contacts, only: [:create]
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
