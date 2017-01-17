0f5aa63aaa38ba3d3c6a1750cb392fd05bf726b2
Kevin Speight <kdspeight@hotmail.co.uk>
2017-01-17 14:44:31 +0000

    Added profiles controller, new view, and link on homepage 2

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,5 +2,7 @@ class ProfilesController < ApplicationController
   # GET to /users/:/user_id/profile/new
   def new
     # Render blank profile details form
+    # in a .html.erb file you can ad debugging
+    # <% debug(params) if Rails.env.development? %>
   end
 end
